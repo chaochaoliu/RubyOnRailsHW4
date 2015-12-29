9e0c7f8dca8cd57afa45471228da8d064df60b45
Kevin <liuchao200240@gmail.com>
2015-12-29 00:16:56 -0800

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ class User < ActiveRecord::Base
 	belongs_to :user_role
 	has_many :posts
 	validates :email, presence: true
+  validates :email, presence: true
 	validates :password, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :email, format: { with: /\w+@\w+\.\w+/,
+    message: "not valid email address" }
 
   def admin?
     self.user_role.name == 'admin'
